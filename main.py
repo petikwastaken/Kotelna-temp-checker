8b0229b8b5539b145ef4027c5400d37fc9383658
petikwastaken <petrsanc8@gmail.com>
2022-11-27 18:46:43 +0100

    FINALIZED
    92 75

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 from urllib.request import urlopen
-
+import os
 link = "http://213.108.160.85:666/"
+# Import the following modules
+import requests
+import json
+import time as t
 
 f = urlopen(link)
 myfile = f.read()
 print(myfile)
 x = str(myfile)
-x = x.split(':')
-print(" ")
-print(x)
+x = x.split(' ')
+
+# kotel: 50, aku: 43
+kotel = x[50]
+aku = x[43].split('%')
+aku = aku[0]
+print("////////////////////////////////////////////////////")
+print(aku)
+print(kotel)
+
+
+ 
+# Function to send Push Notification
+ 
+ 
+def pushbullet_noti(title, body):
+ 
+    TOKEN = 'o.f9BZI9VWMfn1BzKIU3PQHxIPdPKcNwxO'  # Pass your Access Token here
+    # Make a dictionary that includes, title and body
+    msg = {"type": "note", "title": title, "body": body}
+    # Sent a posts request
+    resp = requests.post('https://api.pushbullet.com/v2/pushes',
+                         data=json.dumps(msg),
+                         headers={'Authorization': 'Bearer ' + TOKEN,
+                                  'Content-Type': 'application/json'})
+    if resp.status_code != 200:  # Check if fort message send with the help of status code
+        raise Exception('Error', resp.status_code)
+    else:
+        print('Message sent')
+
+kotelnaData = "KOTEL: " + kotel + "°C⚠!! ACCUM: " + aku + "%⚠!!"
 while(True):
-    kotel = input()
-    acc = input()
-    if(int(kotel) >= 92 or int(acc) >= 80):
-        print("PRETOPENO!!!")
+    f = urlopen(link)
+    myfile = f.read()
+    print(myfile)
+    x = str(myfile)
+    x = x.split(' ')
+    #list navigace:: kotel: 50, aku: 43
+    kotel = x[50]
+    aku = x[43].split('%')
+    aku = aku[0]
+    print(aku)
+    print(kotel)
+    i = 0
+    if(kotel >= 92 or aku >= 75):
+        while(i <= 3):
+            pushbullet_noti("⚠KOTELNA⚠", kotelnaData)
+            t.sleep(0.3)
+            i = i + 1
